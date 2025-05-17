2ddfd43d79024b17ac4ec4e9ca5eae6dc410dce5
shailendras <shailendrashukl@gmail.com>
2025-05-17 23:34:45 +0530

    avoided hard coding by adding topic variable,added link , subject in mail body

--- Main.py
+++ Main.py
@@ -2,16 +2,23 @@ import requests
 from send_email import send_email
 
 api_key = "5098df43930442a8b8019a2e3f52c822"
+topic = "tesla"
 
-url = ("https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&"
-       "apiKey=890603a55bfa47048e4490069ebee18c")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "sortBy=publishedAt&"
+       "apiKey=890603a55bfa47048e4490069ebee18c&"
+       "language=en")
 
 request = requests.get(url)
 content = request.json()
-body = ""
-for article in content["articles"]:
+body =  "Subject : Today's News\n\n"
+for article in content["articles"][:19]:
     if article["description"] is not None:
-      body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+      body = (body + article["title"] + "\n" +
+              article["description"] + "\n" +
+              article["url"] + 2 * "\n")
+
 body = body.encode("utf-8")
 send_email(body)
 
